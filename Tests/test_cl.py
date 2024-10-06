c02f60f7448a0a9d9646e4ba787bf43c14fffe62
hallm27 <hallm6@carleton.edu>
2024-10-05 21:42:49 -0500

    One other test eas added.

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -24,4 +24,15 @@ class TestClass(unittest.TestCase):
          self.assertEqual(output.strip(), "8.78")
          expectedCode.terminate()
 
+
+    def test_get_score_incorrect_title(self):
+         """This tests the edge case for get_score when an invalid title is inputted. An empty string should be retired."""
+         expectedCode = subprocess.Popen(["python3", "ProductionCode/utils.py", "--title", "Space Jam"], 
+                        stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding='utf8')
+         output, err = expectedCode.communicate()
+         self.assertEqual(output.strip(), "")
+         expectedCode.terminate()
+    
+    
+
 unittest.main()
