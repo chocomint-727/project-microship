fb0f6e1faac5d5e41cebf2c9476a50a56ae93fde
Christian Park <christian.park@me.com>
2024-10-05 19:47:50 -0500

    added filter by genres

--- ProductionCode/utils.py
+++ ProductionCode/utils.py
@@ -5,7 +5,19 @@ dataset = open('../Data/anime.csv', newline='')
 
 dataset_read = csv.reader(dataset)
 
-col_titles = dataset_read[0]
-
 for row in dataset_read:
     print(row)
+   
+def filter_by_genres(genres):
+    return_rows = [] # collections of rows to return
+    for row in dataset_read:
+        genres_match = [0] * len(genres) # bitmap for matched genres
+        for i, genre in enumerate(genres):
+            if genre.lower() in row[3].lower():
+                genres_match[i] = 1 # if match, set to 1
+                
+        if sum(genres_match) == len(genres_match): # if all genres are matched
+            return_rows.append(row)
+            
+    return return_rows
+        
