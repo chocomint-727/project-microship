439d4b21acddc5178dd46f449d50023ca40062ff
root <root@Omar>
2024-10-05 19:47:14 -0500

    Made get_genre

--- ProductionCode/utils.py
+++ ProductionCode/utils.py
@@ -1,11 +1,14 @@
 import csv
 
-
-dataset = open('../Data/anime.csv', newline='')
+dataset = open('/root/team-project-microship/Data/anime.csv', newline='')
 
 dataset_read = csv.reader(dataset)
 
-col_titles = dataset_read[0]
+#for row in dataset_read:
+#    print(row)
 
-for row in dataset_read:
-    print(row)
+def get_genre(title):
+    for row in dataset_read:
+        if row[1] == title:
+            print(row[3])
+            return row[3]
