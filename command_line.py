95af4da254c5f98806c20bcaa34a22b0d21ed1be
Christian Park <christian.park@me.com>
2024-10-05 20:06:13 -0500

    added arg parse

--- command_line.py
+++ command_line.py
@@ -1,6 +1,18 @@
 import sys
 import os
+import argparse
 
+parser = argparse.ArgumentParser(
+                    prog='ProgramName',
+                    description='What the program does',
+                    epilog='Text at the bottom of help')
+
+parser.add_argument("--genres", nargs="+")
+parser.add_argument("--title", type=str)
+
+args = parser.parse_args()
+print(args.genres)
+print(args.title)
 
 def filter_by_genres(genres):
     for g in genres:
