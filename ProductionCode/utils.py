564419689dc37fc4b7514c53ddfe8e6de6203da4
root <root@Omar>
2024-10-05 19:52:08 -0500

    Made get_score

--- ProductionCode/utils.py
+++ ProductionCode/utils.py
@@ -4,9 +4,18 @@ dataset = open('/root/team-project-microship/Data/anime.csv', newline='')
 
 dataset_read = csv.reader(dataset)
 
-col_titles = dataset_read[0]
+#for row in dataset_read:
+#    print(row)
+
+def get_score(title):
+    ''' '''
+    for row in dataset_read:
+        if row[1] == title:
+            print(row[2])
+            return row[2]
 
 def get_genre(title):
+    ''' '''
     for row in dataset_read:
         if row[1] == title:
             print(row[3])
