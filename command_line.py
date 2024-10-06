485d580d708b0a75a1d5ab09d7c4a3840e6d67a4
Christian Park <christian.park@me.com>
2024-10-05 20:28:21 -0500

    deleted lines

--- command_line.py
+++ command_line.py
@@ -13,17 +13,7 @@ parser.add_argument("--title", type=str)
 
 args = parser.parse_args()
 
-def filter_by_genres(genres):
-    for g in genres:
-        pass
-
 if args.genres is not None:
     print(filter_by_genres(args.genres))
 elif args.title is not None:
     get_score(args.title)
-
-
-=======
-    print(filter_by_genres(args.genres))
-    
->>>>>>> e9dd4ff4f62f843d7c1404bad5ebd5e9828df6d7
