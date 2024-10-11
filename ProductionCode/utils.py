6c57c6886d5b2fab06c8678ced0248c7f259dd9b
Christian Park <christian.park@me.com>
2024-10-11 10:31:40 -0500

    amended the filter function

--- ProductionCode/utils.py
+++ ProductionCode/utils.py
@@ -13,16 +13,16 @@ def get_score(title):
             print(row[2])
             return row[2]
    
-def filter_by_genres(genres):
+def filter_by_genres(genres : list):
     """ This function returns a list of ids that match all specified genres """
     return_rows = [] # collections of rows to return
-    for row in dataset_read:
+    for ix, row in enumerate(dataset_read[1:]):
         genres_match = [0] * len(genres) # bitmap for matched genres
         for i, genre in enumerate(genres):
             if genre.lower() in row[3].lower():
                 genres_match[i] = 1 # if match, set to 1
-                
+        
         if sum(genres_match) == len(genres_match): # if all genres are matched
-            return_rows.append(int(row[0]))
+            return_rows.append(ix) # this bit of the code has been changed to return the indices of the items instead of their ids
             
     return return_rows
