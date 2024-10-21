b9c81e72f215b44058c87ae0ef1b3b3909a5fc01
hallm27 <hallm6@carleton.edu>
2024-10-20 19:47:56 -0500

    Created table.

---
+++ Data/createtable.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS anime_table;
+
+CREATE TABLE anime_table(
+    Title VARCHAR(100),
+    Score float
+    Genre VARCHAR(125),
+    AnimeType VARCHAR(6),
+    AnimeLength int,
+    AirDate VARCHAR(28),
+    Producers VARCHAR(375),
+    Studio VARCHAR(80),
+    Source VARCHAR(13),
+    Duration VARCHAR(21),
+    Rating VARCHAR(30),
+    Popularity int
+
+)
