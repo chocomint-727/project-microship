b7b46d73f2fb121fb8873a0baed6d3c8fb8cd2f8
osobhy <sobhyo@stearns.mathcs.carleton.edu>
2024-10-21 20:31:06 -0400

    Added datasource.py

---
+++ ProductionCode/datasource.py
@@ -0,0 +1,49 @@
+import psycopg2
+
+import ProductionCode.psqlConfig as config
+
+class DataSource:
+    def __init__(self):
+        '''Constructor that initiates connection to database '''
+        self.connection = self.connect()
+
+    def connect(self):
+        '''Initiates connection to database using information in the psqlConfig.py file.
+        Returns the connection object.'''
+        try:
+            connection = psycopg2.connect(database=config.database, user=config.user, password=config.password, host="localhost")
+        except Exception as e:
+            print("Connection error: ", e)
+            exit()
+        return connection
+
+    def get_all_anime(self):
+        ''' Outputs the entire dataset - currently only includes two columns: title and score '''
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM anime")
+        records = cursor.fetchall()
+        print(records)
+        
+    def get_score_from_title(self, type):
+        ''' Gets score of Anime title input by user '''
+        try:
+            cursor = self.connection.cursor()
+            query = "SELECT score FROM anime WHERE anime_title = %s;"
+            cursor.execute(query, (type,))
+            print(cursor.fetchall())
+
+        except Exception as e:
+            print ("Something went wrong when executing the query: ", e)
+            return None
+        
+    def get_title_from_score(self, type):
+        ''' Gets titles that  match with score input by user. Returns a list of titles '''
+        try:
+            cursor = self.connection.cursor()
+            query = "SELECT * FROM anime WHERE score = %s;"
+            cursor.execute(query, (type,))
+            print(cursor.fetchall())
+
+        except Exception as e:
+            print ("Something went wrong when executing the query: ", e)
+            return None
--- flask_app.py
+++ flask_app.py
@@ -82,4 +82,4 @@ def python_bug(e):
 
 if __name__ == "__main__":
     load_data()
-    app.run()
+    app.run(host='0.0.0.0', port=5125)
