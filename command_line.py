b76519cd3404f6d05824a956811347e79c97e110
osobhy <sobhyo@stearns.mathcs.carleton.edu>
2024-10-21 21:23:39 -0400

    edited commandline

--- command_line.py
+++ command_line.py
@@ -1,7 +1,9 @@
 import sys
 import os
 import argparse
-from ProductionCode.utils import *
+from ProductionCode.datasource import *
+sql = DataSource()
+
 
 """This file parses the command line arguments and provides instructions for what to do next."""
 parser = argparse.ArgumentParser(
@@ -10,13 +12,13 @@ parser = argparse.ArgumentParser(
                     epilog='With the Anime Browser tool, you can find all sorts of useful information on your current (or next!) favorite Anime. Get binging!')
 
 parser.add_argument("--genres", nargs="+")
-"""Usage statement for filter_by_genres: python3 commandline.py --genres [genres to filter by]"""
+"""Usage statement for filter_by_genres: python3 command_line.py --genres [genres to filter by]"""
 parser.add_argument("--title", type=str)
-"""Usage statement for get_score: python3 commandline.py --title [some title]"""
+"""Usage statement for get_score: python3 command_line.py --title [some title]"""
 
 args = parser.parse_args()
 
 if args.genres is not None:
-    print(filter_by_genres(args.genres))
+    print(sql.filter_by_genres(args.genres))
 elif args.title is not None:
-    get_score(args.title)
+    print(sql.get_score_from_title(args.title))
