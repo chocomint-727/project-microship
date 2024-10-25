20c4759066dd45202c20376089d94d1d045c4445
osobhy <sobhyo@stearns.mathcs.carleton.edu>
2024-10-25 11:42:07 -0400

    Made homepage and 404 page more thorough

--- flask_app.py
+++ flask_app.py
@@ -12,7 +12,6 @@ def get_title_info(title):
     genre = sql.get_genre_from_title(title)
     return score, genre
 
-
 def format_html_table(indices):    
     
     ids = [f[0] for f in indices]    
@@ -72,15 +71,15 @@ def format_html_table(indices):
 
 @app.route('/')
 def homepage():
-    ''' Returns what is to be displayed on the homepage '''
+    ''' Returns what is to be displayed on the homepage. '''
     titles = "Cowboy Bebop, Cowboy Bebop: Tengoku no Tobira, Trigun, Witch Hunter Robin, Bouken Ou Beet, Eyeshield 21, Hachimitsu to Clover, Hungry Heart: Wild Striker, Initial D Fourth Stage, Monster"
-    display = "Welcome to the homepage! Type in '/title/(title)/' where (title) is the name of the show to get information about an anime. brackets! <br> It currently only displays title and score. <br><br> Titles you can try: <br> " + titles
-    display += "<br><br>To search for shows by their genres, use /genres! Make sure to include get request arguments with the genres you want to include."
+    display = "Welcome to the homepage! Type in '/title/(title)/' where (title) is the name of the show (without brackets) to get information about an anime! For example, /title/Trigun returns score and genre of an Anime with the title Trigun <br>  <br> Titles you can try: <br> " + titles
+    display += "<br><br>To search for shows by their genres, use/genres?genre=(genres to search by) <br> /genres?genre=Action returns a table of Anime titles whose genre is action <br> /genres?genre=Action&genre=Comedy returns a table of Anime titles whose genre is action AND comedy"
     return display
 
 @app.route('/title/<title>/', strict_slashes = False)
 def display_title_info(title):
-    ''' Displays title info. If title does not exist, say it is not found in dataset '''
+    ''' Displays title info. If title does not exist, say it is not found in dataset. '''
     score, genre = get_title_info(title)
     if score is None or genre is None:
         return f"Title '{title}' not found in the dataset."
@@ -89,15 +88,15 @@ def display_title_info(title):
 
 @app.route("/genres")
 def filter(): 
-    """ Interacts with the filter by genres function. 
+    ''' Interacts with the filter by genres function. 
         Because this function takes an arbitrary amount of arguments,
-        get request parameters are used rather than a route. """   
+        get request parameters are used rather than a route. '''
     query = request.args.getlist("genre") # get all args from the get request
     return format_html_table(sql.filter_by_genres(query)) # return the indices in the list
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return "Page not found. Remember that the URL convention is /title/(name of title) or /genres?genre=genre_to_search_by!"
+    return "Page not found. Remember that the URL convention is /title/(name of title) or /genres?genre=(genres to search by) <br> For example, /title/Trigun returns score and genre of an Anime with the title Trigun <br> /genres?genre=Action returns a table of Anime titles whose genre is action <br> /genres?genre=Action&genre=Comedy returns a table of Anime titles whose genre is action AND comedy!"
 
 @app.errorhandler(500)
 def python_bug(e):
