37676351ed16464b5d43a2385075a3420229b80d
Christian <christian.park@me.com>
2024-11-11 12:00:58 -0500

    added popup menu

---
+++ static/scripts/homepage.js
@@ -0,0 +1,45 @@
+function toggleDiv(id)
+{
+    console.log(id);
+    var div = document.getElementById(id);
+    div.style.display = div.style.display == "none" ? "block" : "none";
+}
+
+document.querySelectorAll('.option').forEach(button =>
+{
+    console.log(button.attributes)
+    button.addEventListener('click', () =>
+    {
+        const isEnabled = button.getAttribute("data-enabled") === true;
+        button.setAttribute("data-enabled", !isEnabled);
+        button.classList.toggle("active");
+
+        let existingInput;
+
+        const tag = button.getAttribute("data-name");
+        try {
+            existingInput = document.querySelector(`input[value=${tag}]`)
+        } catch (error) {
+            console.log("Hidden Input Tag Missing")
+        }
+
+        console.log(tag);
+        console.log(existingInput);
+
+        if (!existingInput)
+        {
+            console.log("Creating Hidden Input");
+            let input = document.createElement('input');
+            input.name = 'genre';
+            input.type = 'hidden';
+            input.value = tag;
+            console.log(input);
+            document.getElementById('searchForm').appendChild(input);
+            console.log(input);
+        } else
+        {
+            console.log("Removing Hidden Input");
+            existingInput.remove();
+        }
+    })
+});
